4b48a314960e88e63b2367e80a4e53b75445d16d
eams1798 <eams1798@gmail.com>
2024-11-11 02:17:52 -0500

    Documentation of the routes

--- src/routes/carRoutes.ts
+++ src/routes/carRoutes.ts
@@ -7,17 +7,25 @@ import {
 import { getCarAnalysis } from '../services/aiService';
 import { FBCarItemScraper } from '../utils/FBCarItemScraper';
 import { CLCarItemScraper } from '../utils/CLCarItemScraper';
-import { Car } from '../interfaces';
+import { Car, DefaultCarFilters } from '../interfaces';
 
 const router = Router();
 
+
+/* Entry point to check if server is running */
 router.get('/', (req: Request, res: Response) => {
   res.send('Hello, world!');
 });
 
+
+/* Endpoint to fetch cars based on filters
+ * 
+ * @param {Object} req - Request object. The body contains the filters. The filters can be found in the interface DefaultCarFilters.
+ * @param {Object} res - Response object
+ */
 router.post('/cars', async (req: Request, res: Response) => {
   try {
-    const filters = req.body;
+    const filters: DefaultCarFilters = req.body;
     console.log('filters: ', filters);
 
     /* TODO */
@@ -40,6 +48,12 @@ router.post('/cars', async (req: Request, res: Response) => {
   }
 });
 
+
+/* Endpoint to fetch cars from Facebook based on url
+ * 
+ * @param {Object} req - Request object. The body contains the url.
+ * @param {Object} res - Response object
+ */
 router.post('/facebook', async (req: Request, res: Response) => {
   try {
     const url = req.body.url;
@@ -51,6 +65,12 @@ router.post('/facebook', async (req: Request, res: Response) => {
   }
 });
 
+
+/* Endpoint to fetch cars from craigslist based on url
+ * 
+ * @param {Object} req - Request object. The body contains the url.
+ * @param {Object} res - Response object
+ */
 router.post('/craigslist', async (req: Request, res: Response) => {
   try {
     const url = req.body.url;
@@ -62,6 +82,12 @@ router.post('/craigslist', async (req: Request, res: Response) => {
   }
 });
 
+
+/* Endpoint to get a car analysis from Claude AI
+ * 
+ * @param {Object} req - Request object. The body contains the details of the car. Details can be found in the interface Car.
+ * @param {Object} res - Response object
+ */
 router.post('/ai-info', async (req: Request, res: Response) => {
   const data: Car = req.body;
   
